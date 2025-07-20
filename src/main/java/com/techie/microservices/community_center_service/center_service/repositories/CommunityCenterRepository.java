558b21d04f7af593fe4d064fdecf20e7d730db64
EudesGomes017 <eudesgomes017@gmail.com>
2025-07-20 09:11:35 -0300

    feat: add updateOccupancy

--- src/main/java/com/techie/microservices/community_center_service/center_service/repositories/CommunityCenterRepository.java
+++ src/main/java/com/techie/microservices/community_center_service/center_service/repositories/CommunityCenterRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface CommunityCenterRepository extends MongoRepository<CommunityCenter, String> {
-    List<CommunityCenter> findByOccupancyGreaterThanEqual(int threshold);
+    List<CommunityCenter> findByMaximumCapacityGreaterThanEqual(int threshold);
 
 
     }
--- src/main/java/com/techie/microservices/community_center_service/center_service/services/CommunityCenterService.java
+++ src/main/java/com/techie/microservices/community_center_service/center_service/services/CommunityCenterService.java
@@ -3,14 +3,17 @@ package com.techie.microservices.community_center_service.center_service.service
 
 import com.techie.microservices.community_center_service.center_service.DTO.CommunityCenterRequestDTO;
 import com.techie.microservices.community_center_service.center_service.DTO.CommunityCenterResponseDTO;
+import com.techie.microservices.community_center_service.center_service.DTO.OccupancyAlertEventDTO;
 import com.techie.microservices.community_center_service.center_service.DTO.convert.CommunityCenterMapper;
 import com.techie.microservices.community_center_service.center_service.models.CommunityCenter;
 import com.techie.microservices.community_center_service.center_service.repositories.CommunityCenterRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CommunityCenterService {
 
     @Autowired
@@ -23,5 +26,27 @@ public class CommunityCenterService {
         CommunityCenter center = CommunityCenterMapper.toEntity(request);
         CommunityCenter salve = repository.save(center);
         return CommunityCenterMapper.toDTO(salve);
+    }
+
+    public CommunityCenter updateOccupancy(String id, int newOccupancy) {
+        CommunityCenter center = repository.findById(id).orElseThrow();
+
+        center.setCurrentOccupation(newOccupancy);
+        CommunityCenter updated = repository.save(center);
+
+        if (updated.getCurrentOccupation() >= updated.getMaximumCapacity()) {
+            // Envia alerta para Kafka
+            OccupancyAlertEventDTO event = OccupancyAlertEventDTO.builder()
+                    .centerId(center.getId())
+                    .name(center.getName())
+                    .location(center.getLocation())
+                    .capacity(center.getMaximumCapacity())
+                    .occupancy(center.getCurrentOccupation())
+                    .build();
+
+            kafkaTemplate.send("center-occupancy-alert", center.getId(), event.toString());
+        }
+
+        return updated;
     }
 }
