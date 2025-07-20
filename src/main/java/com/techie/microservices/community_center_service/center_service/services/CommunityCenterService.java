cedaacb4d8e9317bbdf8d7f25b71f3313609dd06
EudesGomes017 <eudesgomes017@gmail.com>
2025-07-19 21:43:52 -0300

    feat:  create method createCenter

--- src/main/java/com/techie/microservices/community_center_service/center_service/services/CommunityCenterService.java
+++ src/main/java/com/techie/microservices/community_center_service/center_service/services/CommunityCenterService.java
@@ -1,4 +1,27 @@
 package com.techie.microservices.community_center_service.center_service.services;
 
+
+import com.techie.microservices.community_center_service.center_service.DTO.CommunityCenterRequestDTO;
+import com.techie.microservices.community_center_service.center_service.DTO.CommunityCenterResponseDTO;
+import com.techie.microservices.community_center_service.center_service.DTO.convert.CommunityCenterMapper;
+import com.techie.microservices.community_center_service.center_service.models.CommunityCenter;
+import com.techie.microservices.community_center_service.center_service.repositories.CommunityCenterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommunityCenterService {
+
+    @Autowired
+    private CommunityCenterRepository repository;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public CommunityCenterResponseDTO createCenter(CommunityCenterRequestDTO request) {
+        CommunityCenter center = CommunityCenterMapper.toEntity(request);
+        CommunityCenter salve = repository.save(center);
+        return CommunityCenterMapper.toDTO(salve);
+    }
 }
